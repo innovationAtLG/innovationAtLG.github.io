c1979b7480086b73ef61ea5fab2eb9574693e194
Alan Stice <alan.stice@lge.com>
2020-11-09 11:42:38 -0800

    rebuilding site Mon 09 Nov 2020 11:42:38 AM PST

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,29 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+printf "\033[0;32mRunning Hugo...\033[0m\n"
 # Build the project.
 hugo
 
+
+printf "\033[0;32Stashing artifacts...\033[0m\n"
 # Go To Public folder
 mv public deploy_tmp
 
+printf "\033[0;32Cleaning directory...\033[0m\n"
 rm -rf !\("deploy_tmp"\)
 
+printf "\033[0;32Inflating stash...\033[0m\n"
 mv deploy_tmp/* ./
 
-# Add changes to git.
-git add .
+printf "\033[0;32Cleaning stash...\033[0m\n"
+rm -rf deploy_tmp
 
+printf "\033[0;32Adding to git...\033[0m\n"
+# Add changes to git.
+git add --all .
+
+printf "\033[0;32Committing...\033[0m\n"
 # Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
@@ -25,7 +35,9 @@ if [ -n "$*" ]; then
 fi
 git commit -m "$msg"
 
+printf "\033[0;32Pushing...\033[0m\n"
 # Push source and build repos.
 git push origin HEAD:master -f
 
+printf "\033[0;32Resetting...\033[0m\n"
 git reset --hard HEAD~
